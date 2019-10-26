59ded1736ceb105962b396b8adb00d250501400c
Emily Wang <41090322+emilwang11@users.noreply.github.com>
2019-10-26 18:17:09 -0400

    Add files via upload

---
+++ src/views/Input.vue
@@ -0,0 +1,55 @@
+<template>
+<v-container>
+    <v-layout justify-center row wrap>
+        <v-flex xs-12 mt-5>
+            <p>Does the code look right? Make any necessary edits</p>
+        </v-flex>
+    </v-layout>
+    <v-layout>
+        <v-flex  pa-3>
+            <v-text-field
+            outlined
+            solo
+            height="500px"
+            width="400px"
+            color="#313030"
+        ></v-text-field>
+        </v-flex>
+    </v-layout>
+    <v-layout>
+        <v-flex xs-12 pa-2>
+            <v-text-field
+            solo
+            height="98px"
+            label = "Inputs"
+            color="#313030"
+        ></v-text-field>
+        </v-flex>
+    </v-layout>
+    <v-layout>
+        <v-flex>
+            <v-btn large class="button">
+                Submit
+            </v-btn>
+        </v-flex>
+    </v-layout>
+   </v-container>
+</template>
+
+<script>
+export default {
+  name: "Input",
+  data: () => ({
+   code: "[abc]"
+  }),
+  methods: {
+    // onChange(value) {
+    //   this.$emit("change", value);
+    // }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Result.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex xs12>
+        <v-img
+          :src="require('../assets/summary-illustration.png')"
+          class="my-0"
+          contain
+          height= "330px"
+        ></v-img>
+      </v-flex>
+      </v-layout>
+      <v-layout>
+        <v-flex xs12>
+            <p>
+                Code compiled successfully
+                <br>
+                Run time:
+                <br>
+                Outputs:
+            </p>
+            </v-flex >
+        </v-layout>
+        <router-link :to="'/'">
+            <v-btn large class="button">
+             Restart
+             </v-btn>
+         </router-link>
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Result',
+  components: {
+
+  }
+}
+</script>
